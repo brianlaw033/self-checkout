af4b1e0749fe8f2acb32142c1baa050d55de5380
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-12 18:11:23 +0800

    add the product update feature

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model() {
+    return this.store.findAll('product');
+},
+
+  actions: {
+    update(product, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          product.set(key,params[key]);
+        }
+        product.save();
+      });
+    },
+
+    // saveProduct(params) {
+    //   var newProduct = this.store.createRecord('product', params);
+    //   newProduct.save();
+    // },
+  }
 });
