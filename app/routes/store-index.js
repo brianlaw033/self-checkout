fc24f9ddfff6d93d34199b23eeda0687844eb251
Richard-NG <richard.chihong.ng@gmail.com>
2017-03-13 16:56:07 +0800

    new-product component done

--- app/routes/store-index.js
+++ app/routes/store-index.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('user', params.user_id);
-  }
+  },
+  saveProduct(params) {
+    var newProduct = this.store.createRecord('product', params);
+    var shop = params.shop;
+    shop.get('products').addObject(newProduct);
+    newProduct.save().then(function () {
+      return shop.save();
+    });
+  },
 });
