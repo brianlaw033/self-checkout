36a955cb97d454744ea761bc8fad3263aac142e1
brianlaw033 <brian9328hk@gmail.com>
2017-03-14 09:44:39 +0800

    hide product page when not login

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+login: Ember.inject.service(),
   actions: {
     update(product, params) {
       Object.keys(params).forEach(function(key) {
