be84e0803f7a6e79c7a7243d8df8cbb2e497712e
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-15 20:22:50 +0800

    work on customer route

--- app/models/customer.js
+++ app/models/customer.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   name: DS.attr(),
   icon: DS.attr(),
   location: DS.attr(),
-  user: DS.belongsTo('user', { async: true})
+  user: DS.belongsTo('user', { async: true}),
+  products: DS.hasMany('product', {async:true}),
+  sales: DS.hasMany('sale', {async:true})
 });
