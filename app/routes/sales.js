744bea39f3b6089e291fe4512956020deab9df61
brianlaw033 <brian9328hk@gmail.com>
2017-03-15 19:37:23 +0800

    hide everything when not log in

--- app/routes/sales.js
+++ app/routes/sales.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  login: Ember.inject.service(),
   model(params) {
     return Ember.RSVP.hash({
       sales:this.store.findAll('sale'),
