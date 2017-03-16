8abf8043599cfc46e43afe806e9cfdadf536f4c6
brianlaw033 <brian9328hk@gmail.com>
2017-03-16 16:41:54 +0800

    fixing minor , bug

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  card: DS.belongsTo('card', { async: true})
+  card: DS.belongsTo('card', { async: true}),
   user: DS.belongsTo('user', { async: true})
 });
