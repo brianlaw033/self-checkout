69c4d728b2601a181ab8d9e2ab4bacf0e2371098
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-16 16:16:49 +0800

    solved decimals

--- app/components/shopping-cart-tile.js
+++ app/components/shopping-cart-tile.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
 
+  var itemPrice2 = +((price / listprice).toFixed(2));
+  var itemPrice = parseInt(itemPrice2);
+
   actions: {
     removeItem(item) {
       this.get('shoppingCart').remove(item);
