d0898300e6137d9f7b675861d181ad286210493a
Richard-NG <richard.chihong.ng@gmail.com>
2017-03-17 11:54:17 +0800

    solved multiple buttons

---
+++ tests/unit/helpers/get-distance-test.js
@@ -0,0 +1,12 @@
+
+import { getDistance } from 'self-checkout/helpers/get-distance';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get distance');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getDistance([42]);
+  assert.ok(result);
+});
+
