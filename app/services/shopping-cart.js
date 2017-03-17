7e5ca66d93e9ba57e841ac9f8c7f355ebe8ab5d8
Gabriel <gabrielm.jaeger@yahoo.de>
2017-03-17 23:36:23 +0800

    proceed to purchase button fixed

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -26,8 +26,11 @@ export default Ember.Service.extend({
     this.set('totalprice', Math.round(parseFloat(total-item.get('price'))*100)/100);
     var new_quantity = item.get('quantity_selected')-1;
     item.set('quantity_selected',new_quantity);
+    debugger;
     if(item.get('quantity_selected')==0){
       this.get('items').removeObject(item);
+    }
+    if(this.get('items').length==0){
       this.set('canpay',false);
     }
   },
